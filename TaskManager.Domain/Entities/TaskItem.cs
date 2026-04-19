3b881389cb1695b6b66825f8efaf9ddd422faecd
Vitor Santiago <vitor_meyreles@hotmail.com>
2026-04-19 19:23:00 -0300

    feat: add task entity and task status enum

---
+++ TaskManager.Domain/Entities/TaskItem.cs
@@ -0,0 +1,34 @@
+﻿namespace TaskManager.Domain.Entities
+{
+    public class TaskItem
+    {
+        #region Properties
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public DateTime? DueDate { get; private set; }
+        public TaskStatus Status { get; private set; }
+        #endregion
+
+        #region Constructors
+        public TaskItem(string title, string? description, DateTime? dueDate, TaskStatus status)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            Status = status;
+        }
+        #endregion
+
+        #region Public Methods
+        public void Update(string title, string? description, DateTime? dueDate, TaskStatus status)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            Status = status;
+        }
+        #endregion
+    }
+}
---
+++ TaskManager.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        Pending = 1,
+        InProgress = 2,
+        Completed = 3
+    }
+}
