fa79580f7df1f3bf91fb9ae8be9353b28001b784
Vitor Santiago <vitor_meyreles@hotmail.com>
2026-04-20 11:50:10 -0300

    feat: implement repository pattern and unit of work

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Interfaces;
 using TaskManager.Infrastructure.Context;
+using TaskManager.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Configura o Banco em Memória
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("TaskManagerDb"));
+
+// 2. Registra o Repositório e Unit of Work (Padrão Híbrido)
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// 4. Configura Controllers e Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
